dfe72b9b0b77a8702c66ba26bebcea9fabadf1b9
Nick Damianos <nikolaos_damianos_1907@hotmail.com>
2024-03-26 20:46:16 +0200

    first commit

---
+++ Simple_TtS/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace WindowsFormsApplication8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cb = new MetroFramework.Controls.MetroComboBox();
+            this.metroDateTime1 = new MetroFramework.Controls.MetroDateTime();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.metroButton3 = new MetroFramework.Controls.MetroButton();
+            this.metroButton4 = new MetroFramework.Controls.MetroButton();
+            this.txt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // cb
+            // 
+            this.cb.FormattingEnabled = true;
+            this.cb.ItemHeight = 23;
+            this.cb.Items.AddRange(new object[] {
+            "Male",
+            "Female",
+            "Neutral"});
+            this.cb.Location = new System.Drawing.Point(492, 23);
+            this.cb.Name = "cb";
+            this.cb.Size = new System.Drawing.Size(121, 29);
+            this.cb.TabIndex = 0;
+            this.cb.UseSelectable = true;
+            this.cb.SelectedIndexChanged += new System.EventHandler(this.cb_SelectedIndexChanged);
+            // 
+            // metroDateTime1
+            // 
+            this.metroDateTime1.Enabled = false;
+            this.metroDateTime1.Location = new System.Drawing.Point(635, 23);
+            this.metroDateTime1.MinimumSize = new System.Drawing.Size(0, 29);
+            this.metroDateTime1.Name = "metroDateTime1";
+            this.metroDateTime1.Size = new System.Drawing.Size(224, 29);
+            this.metroDateTime1.TabIndex = 1;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(492, 286);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(75, 23);
+            this.metroButton1.TabIndex = 2;
+            this.metroButton1.Text = "Talk";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(589, 286);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(75, 23);
+            this.metroButton2.TabIndex = 3;
+            this.metroButton2.Text = "Pause";
+            this.metroButton2.UseSelectable = true;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // metroButton3
+            // 
+            this.metroButton3.Location = new System.Drawing.Point(690, 286);
+            this.metroButton3.Name = "metroButton3";
+            this.metroButton3.Size = new System.Drawing.Size(75, 23);
+            this.metroButton3.TabIndex = 4;
+            this.metroButton3.Text = "Resume";
+            this.metroButton3.UseSelectable = true;
+            this.metroButton3.Click += new System.EventHandler(this.metroButton3_Click);
+            // 
+            // metroButton4
+            // 
+            this.metroButton4.Location = new System.Drawing.Point(784, 286);
+            this.metroButton4.Name = "metroButton4";
+            this.metroButton4.Size = new System.Drawing.Size(75, 23);
+            this.metroButton4.TabIndex = 5;
+            this.metroButton4.Text = "Save";
+            this.metroButton4.UseSelectable = true;
+            this.metroButton4.Click += new System.EventHandler(this.metroButton4_Click);
+            // 
+            // txt
+            // 
+            this.txt.Location = new System.Drawing.Point(23, 63);
+            this.txt.Multiline = true;
+            this.txt.Name = "txt";
+            this.txt.Size = new System.Drawing.Size(836, 217);
+            this.txt.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 332);
+            this.Controls.Add(this.txt);
+            this.Controls.Add(this.metroButton4);
+            this.Controls.Add(this.metroButton3);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.metroDateTime1);
+            this.Controls.Add(this.cb);
+            this.Name = "Form1";
+            this.Text = "Text to Speech by Nikolaos Damianos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroComboBox cb;
+        private MetroFramework.Controls.MetroDateTime metroDateTime1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private MetroFramework.Controls.MetroButton metroButton3;
+        private MetroFramework.Controls.MetroButton metroButton4;
+        private System.Windows.Forms.TextBox txt;
+    }
+}
+
---
+++ Simple_TtS/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.IO;
+
+namespace WindowsFormsApplication8
+{
+    public partial class Form1 :MetroFramework.Forms.MetroForm
+    {
+        SpeechSynthesizer voice;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void metroTile1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            voice = new SpeechSynthesizer();
+            voice.SelectVoiceByHints(VoiceGender.Male);
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            
+                switch (cb.SelectedIndex) {
+                    case 0:
+                        voice.SelectVoiceByHints(VoiceGender.Male);
+                        break;
+                    case 1:
+                        voice.SelectVoiceByHints(VoiceGender.Female);
+                        break;
+                    case 2:
+                        voice.SelectVoiceByHints(VoiceGender.Neutral);
+                        break;
+                    default:
+                        voice.SelectVoiceByHints(VoiceGender.Male);
+                        break;
+                }
+            
+            voice.SpeakAsync(txt.Text);
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                voice.Pause();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void metroButton3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                voice.Resume();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void metroButton4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SaveFileDialog svf = new SaveFileDialog()) {
+                    svf.Filter = "Wav files|*.wav";
+
+                    if (svf.ShowDialog() == DialogResult.OK) {
+                        FileStream fs = new FileStream(svf.FileName,FileMode.Create, FileAccess.Write);
+                        voice.SetOutputToWaveStream(fs);
+                        voice.Speak(txt.Text);
+                    }
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void cb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (cb.SelectedIndex) {
+                case 0:
+                    voice.SelectVoiceByHints(VoiceGender.Male);
+                    break;
+                case 1:
+                    voice.SelectVoiceByHints(VoiceGender.Female);
+                    break;
+                case 2:
+                    voice.SelectVoiceByHints(VoiceGender.Neutral);
+                    break;
+            }
+        }
+    }
+}
